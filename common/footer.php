46b6fe396786ad7ffaec6218ed7cc8acdd1581a7
Michael Tedeschi <mike@interactivemechanics.com>
2013-06-14 20:22:26 -0400

    Clean up files, add bootstrap, build header

--- common/footer.php
+++ common/footer.php
@@ -1,15 +1,7 @@
-    </div>
-	<div id="footer-text">
+	<footer>
 	    <p><?php echo get_theme_option('Footer Text'); ?> &copy; <?php echo date('Y'); ?></p>
-	</div>
+	</footer>
 	<?php fire_plugin_hook('public_footer'); ?>
-</div>
-<script type="text/javascript">
-    jQuery(document).ready(function () {
-        Omeka.showAdvancedForm();        
-        Omeka.moveNavOnResize();        
-        Omeka.mobileMenu();        
-    });
-</script>
+</div> <!-- end .container -->
 </body>
 </html>
